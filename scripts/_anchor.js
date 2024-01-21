48862fc87dd48faa0863c14d0d6ba8bf0dc55376
Anton Shchukin <a.a.shchukin@gmail.com>
2024-01-21 20:33:49 +0700

    header

--- scripts/_anchor.js
+++ scripts/_anchor.js
@@ -1,12 +1,12 @@
 (function($) {
 
-    var scrollSpeedPer100ms = 200; /* 200px per 100ms */
+    const scrollSpeedPer100ms = 200; /* 200px per 100ms */
 
     $('.anchor').on('click', function (event) {
         event.preventDefault();
 
         const $target = $( $(this).attr('href') );
-        const headerHeight = $('.elements-header').css('position') === 'fixed' ? $('.header').outerHeight() : 0;
+        const headerHeight = $('.elements-header').css('position') === 'fixed' ? $('.elements-header').outerHeight() : 0;
         const scrollDistance = $target.offset().top - $(this).offset().top - headerHeight;
         const animationDuration = Math.max(Math.abs(scrollDistance) / scrollSpeedPer100ms * 100, 300); /* Not less than 300ms (for short distance) */
         const scrollCoordinate = $target.offset().top - headerHeight;
