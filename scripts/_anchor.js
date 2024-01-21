973664383f82ed9ccf734e1fa3b70107811266f5
Anton Shchukin <a.a.shchukin@gmail.com>
2024-01-21 20:32:10 +0700

    anchor

--- scripts/_anchor.js
+++ scripts/_anchor.js
@@ -1,22 +1,19 @@
 (function($) {
 
-
     var scrollSpeedPer100ms = 200; /* 200px per 100ms */
 
     $('.anchor').on('click', function (event) {
         event.preventDefault();
 
-
-        var $target = $( $(this).attr('href') );
-        var headerHeight = $('.header').length ? $('.header').outerHeight() : 0;
-        var scrollDistance = $target.offset().top - $(this).offset().top - headerHeight;
-        var animationDuration = Math.max(Math.abs(scrollDistance) / scrollSpeedPer100ms * 100, 300); /* Not less than 300ms (for short distance) */
-        var scrollGap = $(this).attr('data-anchor-gap') ? +$(this).attr('data-anchor-gap') : 0;
-        var scrollCoordinate = $target.offset().top - headerHeight;
+        const $target = $( $(this).attr('href') );
+        const headerHeight = $('.elements-header').css('position') === 'fixed' ? $('.header').outerHeight() : 0;
+        const scrollDistance = $target.offset().top - $(this).offset().top - headerHeight;
+        const animationDuration = Math.max(Math.abs(scrollDistance) / scrollSpeedPer100ms * 100, 300); /* Not less than 300ms (for short distance) */
+        const scrollCoordinate = $target.offset().top - headerHeight;
 
         if( $target.length ) {
             $('html, body').animate({
-                scrollTop: Math.ceil(scrollCoordinate - scrollGap)
+                scrollTop: scrollCoordinate
             }, animationDuration);
         }
     });
